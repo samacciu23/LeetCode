003ab8f1194a8e19f3c247ddacb26f27c2289c99
Samuele <sd6049m@gre.ac.uk>
2024-07-24 11:11:31 +0100

    Add StudentsAndExaminations.sql

---
+++ problems/sql/cross join/StudentsAndExaminations.sql
@@ -0,0 +1,5 @@
+select st.student_id, st.student_name, su.subject_name, count(ex.subject_name) as attended_exams
+from Students st cross join Subjects su left outer join Examinations ex
+on st.student_id = ex.student_id and su.subject_name = ex.subject_name
+group by st.student_id, st.student_name, su.subject_name
+order by st.student_id, su.subject_name;
